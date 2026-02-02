d7818e2f83218655fdae2638d2350f534176dadd
Shivank Bhasin <shivank2609@gmail.com>
2026-02-02 21:55:09 +0530

    Created income

---
+++ frontend/src/app/features/income/income.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../../layout/navbar.component';
+import { IncomeService } from '../../core/services/income.service';
+import { IncomeOverview } from '../../models/income.model';
+
+@Component({
+  selector: 'app-income',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
+  template: './income.component.html'
+})
+export class IncomeComponent implements OnInit {
+  income: IncomeOverview | null = null;
+  loading = true;
+
+  constructor(private incomeService: IncomeService) {}
+
+  ngOnInit() {
+    this.loadIncome();
+  }
+
+  loadIncome() {
+    this.incomeService.getIncomeOverview().subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.income = response.data;
+        }
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+}
